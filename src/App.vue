ac864908e2a0e49d9732cc7d66ce50599d4e5b0f
keavvy <keavvywai@163.com>
2022-09-27 15:20:12 +0800

    d

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ name }}</h1>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  setup() {
+    const name = "keavvy";
+    return {
+      name,
+    };
+  },
+});
+</script>
+
